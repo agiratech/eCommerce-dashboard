f35b8e488d577417133f41b0a73b7af92dfda1d3
agiratech-reddysai <reddysai@agiratech.com>
2016-07-19 18:26:55 +0530

    converted to bootstrap

--- public/javascripts/angular/controllers/products-ctrl.js
+++ public/javascripts/angular/controllers/products-ctrl.js
@@ -8,14 +8,27 @@ app.controller('ProductList', function($uibModal,$scope,myFactory,$location) {
   $scope.show = function(id) {
       angular.forEach($scope.productList, function(value, key){
         if (value.retailerProductConfigurationId === id) {
-          $scope.productDetails = value;
-          console.log(value)
           var modalInstance = $uibModal.open({
             templateUrl: 'templates/productDetails.html',
+            controller: 'ProductDetails',
+            size: 'lg',
+            resolve: {
+                       param: function () {
+                           return {'productDetails' : value };
+                       }
+                      }
           });
         };
       });
     };
+
   $scope.gridOptionForProducts = {data: 'productList', columnDefs: [
       { name: 'productName'},{ name: 'brand'},{ name: 'color'},{ name: 'status'},{ name: 'shopperType'},{ name: 'salePrice'},{ name: 'shippingFee'},{name: 'Events', cellTemplate: '<div><button class = "btn btn-xs btn-primary" ng-click="grid.appScope.show(row.entity.retailerProductConfigurationId)" type="button"> <i class="fa fa-eye" aria-hidden="true"> </i> </button></div>'}],};
+});
+
+app.controller('ProductDetails', function($scope,$uibModalInstance,myFactory,param) {
+  $scope.productDetails = param.productDetails;
+  $scope.close = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
 });
