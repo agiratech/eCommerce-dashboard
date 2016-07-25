553ad1926a91c09c8f71ad3c96a729f7ed5fde9b
Kumaresan <kumaresan@agiratech.com>
2016-07-25 14:13:00 +0530

    crawl frequency create is completed

--- public/javascripts/angular/controllers/crawlFrequencyConfiguration-ctrl.js
+++ public/javascripts/angular/controllers/crawlFrequencyConfiguration-ctrl.js
@@ -26,7 +26,9 @@ app.controller('CrawlFrequencyConfigurationList', function($uibModal,$scope,myFa
 });
 
 app.controller('CreateCrawlFrequencyConfig', function($scope, $uibModalInstance,myFactory) {
-  $scope.tags = "";
+  // $scope.tags = {};
+  console.log($scope.tags)
+  console.log(8888888888888888)
   $scope.crawlFreq = {
     "spiderId": "",
     "name": "",
@@ -37,9 +39,16 @@ app.controller('CreateCrawlFrequencyConfig', function($scope, $uibModalInstance,
     "minutes": "",
     "disabled": "",
     "priority": "",
-    "addtags" :$scope.tags.split(",")
+    "addtags" : ""
+    // "addtags" :$scope.tags.split(",")
 
   };
+  myFactory.getData("spiders").then(function(response) {
+          $scope.crawlFreqData = response.data.spiders;
+        }, function(response) {
+          alert(response.status);
+          alert(response.data);
+        });
   $scope.months = { Every_months:"*",January:"1",February:"2",March:"3",April:"4",May:"5",June:"6",July:"7",August:"8",September:"9",October:"10",November:"11",December:"12"}
   $scope.days = {Every_days:"*",Monday:"1",Tuesday:"2",Wednesday:"3",Thursday:"4",Friday:"5",Saturday:"6",Sunday:"7"}
   $scope.daysofMonth = {Every_days:"*",1:"1",2:"2",3:"3",4:"4",5:"5",6:"6",7:"7",8:"8",9:"9",10:"10",11:"11",12:"12",13:"13",14:"14",15:"15",16:"16",17:"17",18:"18",19:"19",20:"20",21:"21",22:"22",23:"23",24:"24",25:"25",26:"26",27:"27",28:"28",29:"29",30:"30",31:"31"}
@@ -47,14 +56,6 @@ app.controller('CreateCrawlFrequencyConfig', function($scope, $uibModalInstance,
   $scope.close = function () {
     $uibModalInstance.dismiss('cancel');
   };
-  myFactory.getData("spiders").then(function(response) {
-          $scope.crawlFreqData = response.data.spiders;
-          // console.log($scope.crawlFreq)
-          // console.log(565685)
-        }, function(response) {
-          alert(response.status);
-          alert(response.data);
-        });
   $scope.spiderData = function(id) {
    myFactory.getData("spider/"+ id).then(function(response) {
           $scope.testdata = response.data.spider;
@@ -66,11 +67,11 @@ app.controller('CreateCrawlFrequencyConfig', function($scope, $uibModalInstance,
         });
     };
   $scope.createCrawlFreq = function() {
-    console.log($scope.crawlFreq.name)
-
-    var parameter = {"spiderId":parseInt($scope.crawlFreq.spiderId,10),"name":$scope.crawlFreq.name,"month":$scope.crawlFreq.month,"day":$scope.crawlFreq.day,"dayOfMonth":$scope.crawlFreq.dayOfMonth,"hour":$scope.crawlFreq.hour,"minutes":$scope.crawlFreq.minutes,"disabled":$scope.crawlFreq.disabled,"priority":parseInt($scope.crawlFreq.priority),"addtags":$scope.crawlFreq.addtags};
+    var parameter = {"spiderId":parseInt($scope.crawlFreq.spiderId,10),"name":$scope.crawlFreq.name,"month":$scope.crawlFreq.month,"day":$scope.crawlFreq.day,"dayOfMonth":$scope.crawlFreq.dayOfMonth,"hour":$scope.crawlFreq.hour,"minutes":$scope.crawlFreq.minutes,"disabled":$scope.crawlFreq.disabled,"priority":parseInt($scope.crawlFreq.priority),"addtags":$scope.crawlFreq.addtags.split(",")};
     console.log($scope.retailer)
-    console.log(parameter);
+    console.log($scope.crawlFreq.addtags)
+    console.log(parameter.addtags);
+    console.log(66666666)
     myFactory.createData("crawlFrequencyConfiguration",parameter).then(function(response) {
     console.log(response.data);
     $scope.close();
